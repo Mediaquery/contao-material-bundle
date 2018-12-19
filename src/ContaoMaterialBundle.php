23d35f36839d000b438f8788e070e85c377a7e4d
Mediaquery <jeromelewis@gmx.de>
2018-12-19 15:57:34 +0100

    Version 0.0.1

--- src/ContaoMaterialBundle.php
+++ src/ContaoMaterialBundle.php
@@ -8,10 +8,10 @@
  * @license LGPL-3.0-or-later
  */
 
-namespace Mediaquery\ContaoMaterialBundle;
+namespace Mediaquery\ContaoMaterialBundle2;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class ContaoMaterialBundle extends Bundle
+class ContaoMaterialBundle2 extends Bundle
 {
 }
